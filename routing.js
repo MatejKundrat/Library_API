360da4ccb0331d348df453f2a52cfa24517131f2
MatejKundrat <74267171+MatejKundrat@users.noreply.github.com>
2021-04-07 23:14:23 +0200

    zacomentovalny routing

---
+++ routing.js
@@ -0,0 +1,42 @@
+    // const express = require('express');
+    // const app = express();
+    // const router = express.Router();
+
+
+    // app.get('/book', (req, res) => {
+    //     return res.send(library);
+    //   });
+      
+      
+    //   app.get('/book/tags', (req, res) => {
+    //     var arrTag = [];
+    //     for ( var book of library) {
+    //       arrTag.push(book.tags)
+    //     }
+    //     return res.send(arrTag);
+        
+    //   });
+      
+    //   app.get('/book/:bookId', (req, res) => {
+    //     const bookId = (req.params.bookId);
+    //     return res.send(library.find(book => book.id == bookId));
+        
+    //   });
+      
+    //   app.post('/book', (req, res) => {
+    //     library.push(req.body);
+    //     return res.send(library);
+    //   });
+    //   app.delete('/book/:bookId', (req, res) => {
+    //     const bookId = (req.params.bookId);
+    //     library.splice();
+    //     return res.send(library.find(book => book.id == bookId));
+        
+    //   }); //zatial nefunguje
+      
+      
+    //   app.put('/book/bookID', function(req, res){
+    //     data[params['Book']]['completed'] = req.data.completed;
+
+    //     });
+
